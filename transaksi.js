853f7312876ca1a29a06fda0d9a49da460757ba7
mufasintya <141395501+mufasintya@users.noreply.github.com>
2023-11-17 20:59:28 +0700

    mengulangi transaksi
    - menambah const ulang = document.querySelector(".reset");
    - menambah confirm untuk mengulangi transaksi
    - menambah function ulangi untuk mereset value jumlah dan hasil

--- transaksi.js
+++ transaksi.js
@@ -3,9 +3,24 @@ const jumlahApel = document.querySelector(".jumlah_apel");
 const jumlaAnggur = document.querySelector(".jumlah_anggur");
 const hasil = document.querySelector("#total");
 
+const ulang = document.querySelector(".reset");
 const proses = document.querySelector('input[type="button"]');
-proses.addEventListener("click", function () { //addEventListener dijalankan d=sesuai perintah seperti "click"
+proses.addEventListener("click", function () {
+  //addEventListener dijalankan d=sesuai perintah seperti "click"
   anggur = jumlaAnggur.value * 20000;
   apel = jumlahApel.value * 23000;
   hasil.value = apel + anggur; //untuk menampilkan nilai di total
 });
+
+ulang.addEventListener("click", function () {
+  const tanya = confirm("Apakah anda yakin ingin mengulangi transaksi?");
+  if (tanya) {
+    ulangi();
+  }
+});
+
+function ulangi() {
+  jumlaAnggur.value = "";
+  jumlahApel.value = "";
+  hasil.value = "";
+}
